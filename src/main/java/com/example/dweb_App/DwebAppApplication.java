d875a073a34f0197694489f911aed0a82ec76d08
Saadiaboussiar <boussiarsaadia@gmail.com>
2025-07-22 19:00:03 +0100

    initial commit deweb-App backend

---
+++ src/main/java/com/example/dweb_App/DwebAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.dweb_App;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DwebAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DwebAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/dweb_App/DwebAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dweb_App;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DwebAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
